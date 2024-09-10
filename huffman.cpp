fab3385145ebde3dcd5ba2e47a76ae51ffc728bb
JustGoosiest <muxiaolong28@gmail.com>
2024-09-11 00:25:11 +0300

    implemented min heap and made some improvements in the algorithm

--- huffman.cpp
+++ huffman.cpp
@@ -1,20 +1,82 @@
 // INITIALIZE POINTERS TO NULL IF YOU'RE NOT GONNA FUCKING USE THEM
+
 #include <bits/stdc++.h>
+
 #define sz(s) (int)(s.size())
 
 using namespace std;
 using ll = long long;
 
 struct node{
-	char val;
-	ll freq;
+	char val = '\0';
+	ll freq = 0;
 	node *left = nullptr, *right = nullptr;
-	node(char val, ll freq) : val(val), freq(freq){}
+	node() {}
+	node(char val, ll freq) : val(val), freq(freq) {}
+	node(char val, ll freq, node *left, node *right) : val(val), freq(freq), left(left), right(right) {}
 };
 
-struct cmp {
-    bool operator()(const node* lhs, node* rhs) const { return lhs->freq > rhs->freq; }
+class minHeap{
+private:
+	size_t size = 0, capacity = 1;
+	node **arr = nullptr;
+
+	int p(int i){ return ((i - 1) >> 1); }
+	int l(int i){ return ((i << 1) + 1); }
+	int r(int i){ return ((i << 1) + 2); }
+
+	void bubbleUp(int i){
+		if(i <= 0 || arr[i]->freq >= arr[p(i)]->freq)
+			return;
+		swap(arr[i], arr[p(i)]);
+		bubbleUp(p(i));
+	}
+
+	void heapify(int i){
+		int left = l(i);
+		int right = r(i);
+		int mn = i;
+		if(left < size && arr[left]->freq < arr[i]->freq)
+			mn = left;
+		if(right < size && arr[right]->freq < arr[mn]->freq)
+			mn = right;
+		if(mn != i){
+			swap(arr[mn], arr[i]);
+			heapify(mn);
+		}
+	}
+
+public:
+	
+	minHeap(){ arr = new node*[capacity]; }
+	
+	int len() { return size; }
+
+	node* peek() { return size > 0 ? arr[0] : nullptr; }
+
+	node* extract() {
+		if(size <= 0)
+			return nullptr;
+		node* ret = arr[0];
+	    arr[0] = arr[--size];
+		heapify(0);
+		return ret;
+	}
+	
+	void insert(node *k) {
+		if(size == capacity){
+			capacity <<= 1;
+			node **dummy = new node*[capacity];
+			for(int i = 0; i < size; i++)
+				dummy[i] = arr[i];
+			delete[] arr;
+			arr = dummy;
+		}
+		arr[size++] = k;
+		bubbleUp(size - 1);
+	}
 };
+
 
 class HuffmanTree{
 
@@ -22,14 +84,17 @@ private:
 	string path;
 	node *root = nullptr;
 	unordered_map<char, string> codes;
-
+	unordered_map<char, size_t> freq;
+	uint64_t outputBitLength = 0, inputLength = 0;
+	
 	void makeCodes(node* root){
 		if(root->left == nullptr && root->right == nullptr){
 		    codes[root->val] = path;
 			cout << root->val << ' ' << path << '\n';
 			return;
 		}
-	    path += '0', makeCodes(root->left), path.pop_back();
+
+		path += '0', makeCodes(root->left), path.pop_back();
 	    path += '1', makeCodes(root->right), path.pop_back();
 	}
 
@@ -42,119 +107,114 @@ private:
 	
 	char toBin(deque<char>& buf){
 		char ret = 0x00;
-
 		for(int i = 0; i < 8; i++){
-			if(buf[0] == '1')
-				ret |= (1 << (7 - i));
+			ret |= ((buf[0] == '1') << (7 - i));
 			buf.pop_front();
 		}
-
 		return ret;
 	}
 
 public:
-
+	HuffmanTree(ifstream& inputFile) {
+		char byte;
+		while(inputFile.read(&byte, 1)){
+			freq[byte]++;
+			inputLength++;
+		}
+		inputFile.clear();
+		inputFile.seekg(0);
+	}
+	
 	string getCode(char s){ return codes[s]; }
 
-	void buildTree(unordered_map<char, ll>& freq){
+	void buildTree(){
+		minHeap heap;
 
-		priority_queue<node*, vector<node*>, cmp> q;
+		for(auto &[f, s] : freq)
+		    heap.insert(new node(f, s));
 
-		for(auto &[f, s] : freq){
-			q.push(new node(f, s));
+		while(heap.len() > 1){
+			node *l = heap.extract();
+			node *r = heap.extract();
+			heap.insert(new node('\0', l->freq + r->freq, l, r));
 		}
 
-		while(q.size() > 1){
-			node* left = q.top(); q.pop();
-			node* right = q.top(); q.pop();
-			node* parent = new node('\0', left->freq + right->freq);
-			parent->left = left, parent->right = right;
-			q.push(parent);
-		}
-
-	    makeCodes(root = q.top());
+		root = heap.extract();
+		makeCodes(root);
 
 		return;
 	}
 
 	void encode(ifstream& inputFile, ofstream& outputFile){
+		char byte, toWrite;
+		deque<char> buf;
+
+	    while(inputFile.read(&byte, 1)){
+		    add(getCode(byte), buf);
+			while(sz(buf) >= 8){
+				toWrite = toBin(buf);
+			    outputFile.write(&toWrite, 1);
+				outputBitLength += 8;
+			}
+		}
+
+		outputBitLength += sz(buf);
+		
+		while(sz(buf) < 8)
+			buf.emplace_back('0');
+
+		toWrite = toBin(buf);
+		outputFile.write(&toWrite, 1);
+
+	    for(uint64_t mask = (0xFFULL << 56), shift = 56; mask > 0; mask >>= 8, shift -= 8){
+			toWrite = (mask & outputBitLength) >> shift;
+			outputFile.write(&toWrite, 1);
+		}
+
 		inputFile.clear();
 		inputFile.seekg(0);
 
-		char byte;
-		deque<char> buf;
-		
-		while(inputFile.read(&byte, 1)){
-			
-		    add(getCode(byte), buf);
-
-			while(sz(buf) >= 8){
-				char toWrite = toBin(buf);
-			    outputFile.write(&toWrite, 1);
-			}
-			
-		}
-
-		char padding = 0;
-		while(sz(buf) < 8){
-			buf.emplace_back('0');
-			padding++;
-		}
-		outputFile.write(&padding, 1);
-		
 		return;
 	}
-	
 };
 
 
 signed main(int args, char* argv[]){
-	
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	vector<string> arg;
 
 	for(int i = 0; i < args; i++)
 		arg.emplace_back(argv[i]);
 
-
 	if(sz(arg) != 4 || (arg[2] != "e" && arg[2] != "d")) {
 		cerr << "usage:\n./huffman input e\\d output\n";
 		return -1;
 	}
 
-
 	ifstream inputFile(arg[1], ios::binary);
-
 
 	if(!inputFile) {
 		cerr << "error opening file\n";
 		return -2;
 	}
 
+	string outputPath = arg[1];
+
+	while(!outputPath.empty() && outputPath.back() != '/')
+		outputPath.pop_back();
+
+	outputPath += arg[3] + ".bin";
+
+	ofstream outputFile(outputPath, ios::binary);
 
 	if(arg[2] == "e") {
-
-		string outputPath = arg[1];
-		
-		while(outputPath.back() != '/')
-			outputPath.pop_back();
-
-		outputPath += arg[3] + ".bin";
-		
-		char byte;
-		unordered_map<char, ll> freq;
-		ofstream outputFile(outputPath, ios::binary);
-
-		while(inputFile.read(&byte, 1))
-			freq[byte]++;
-
-		HuffmanTree huffman;
-		huffman.buildTree(freq);
-		// huffman.encodeTree(ouputFile);
+		HuffmanTree huffman(inputFile);
+		huffman.buildTree();
+		// huffman.encodeTree(ouputFile); // will implement tree encoding soon
 		huffman.encode(inputFile, outputFile);
-
 		inputFile.close();
 		outputFile.close();
 
